f511d46c393707cb1dd9dcb3f4d1c0ba35604991
Łukasz <Lukasz19281@gmail.com>
2019-12-11 12:29:22 +0100

    Solved 2019 day11

--- 2019/day11/task2/main.py
+++ 2019/day11/task2/main.py
@@ -1,7 +1,6 @@
 from copy import copy
 from enum import Enum, IntEnum
 
-
 class IntcodeCodes(Enum):
     HALT = 0,
     NO_INPUT = 1,
@@ -151,9 +150,9 @@ class Direction(Enum):
 
 
 comp = IntcodeComputer([int(x) for x in open('input', 'r').read().split(',')])
-whites = set()
 painted = set()
 loc = (0, 0)
+whites = {loc}
 direction = Direction.UP
 while True:
     s, r = comp.run(color_id(whites, loc))
@@ -175,4 +174,15 @@ while True:
             d = Rotation(r)
             direction = direction.rotate(d)
             loc = (loc[0] + direction[0], loc[1] + direction[1])
-print(len(painted))
+
+minx, maxx, miny, maxy = min([a[0] for a in whites]), max([a[0] for a in whites]), min([a[1] for a in whites]), max([a[1] for a in whites])
+width = maxx - minx + 1
+height = maxy - miny + 1
+board = [[0] * width for _ in range(height)]
+for w in whites:
+    board[w[1] - miny][w[0] - minx] = 1
+board = list(reversed(board))
+for i in range(len(board)):
+    for j in range(len(board[i])):
+        print('#' if board[i][j] == 1 else ' ', end='')
+    print()
