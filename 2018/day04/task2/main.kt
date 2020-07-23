f8ff7e18ede4f4e59fb2f1f0e47ead142250ef1b
Lukasz1928 <Lukasz19281@gmail.com>
2020-07-23 20:21:44 +0200

    Solved 2018 day04

--- 2018/day04/task2/main.kt
+++ 2018/day04/task2/main.kt
@@ -2,7 +2,6 @@ import java.io.File
 import java.io.InputStream
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.time.temporal.ChronoUnit
 
 fun readInput(): List<Record> {
     val inputStream: InputStream = File("input").inputStream()
@@ -56,9 +55,14 @@ fun sortRecords(records: List<Record>): List<Record> {
     return records.sortedWith(compareBy {it.date})
 }
 
-fun findGuardsSleepTime(records: List<Record>): Map<Int, Int> {
+fun findGuardsSleepTime(records: List<Record>): Map<Int, List<Int>> {
     val guardIds = records.map { it.guardId }.filter { it > 0 }.toSet()
-    val times = guardIds.map { it to 0 }.toMap().toMutableMap()
+    val times = guardIds.map { it to listOf<Int>().toMutableList() }.toMap().toMutableMap()
+    for(id in guardIds) {
+        for(i in 0..59) {
+            times[id]!!.add(0)
+        }
+    }
     var currentGuard = -1
     var fallAsleepTime: LocalDateTime = LocalDateTime.now()
     for(rec in records) {
@@ -69,49 +73,37 @@ fun findGuardsSleepTime(records: List<Record>): Map<Int, Int> {
             fallAsleepTime = rec.date
         }
         if(rec.event == Event.WAKE_UP) {
-            val sleepTime = fallAsleepTime.until(rec.date, ChronoUnit.MINUTES).toInt()
-            times[currentGuard] = times[currentGuard]!! + sleepTime
+            val startMin = fallAsleepTime.minute
+            val endMin = rec.date.minute - 1
+            for(m in startMin..endMin) {
+                times[currentGuard]!![m] = times[currentGuard]!![m] + 1
+            }
         }
     }
     return times
 }
 
-fun findGuardWithMostMinutesSlept(times: Map<Int, Int>): Int {
-    return times.toList().maxBy { it.second }!!.first
-}
-
-fun findGuardsMostSleepyMinute(records: List<Record>, guardId: Int): Int {
-    var minutes = listOf<Int>().toMutableList()
-    for(i in 0..59) {
-        minutes.add(0)
-    }
-    var currentGuard = -1
-    var fallAsleepTime: LocalDateTime = LocalDateTime.now()
-    for(rec in records) {
-        if(rec.event == Event.BEGIN_SHIFT) {
-            currentGuard = rec.guardId
-        }
-        if(rec.event == Event.FALL_ASLEEP) {
-            fallAsleepTime = rec.date
-        }
-        if(rec.event == Event.WAKE_UP) {
-            if(currentGuard == guardId) {
-                val startMin = fallAsleepTime.minute
-                val endMin = rec.date.minute
-                for(m in startMin until endMin) {
-                    minutes[m] = minutes[m] + 1
-                }
+fun calculateResult(sleepTimes: Map<Int, List<Int>>): Int {
+    var maxGuard: Int = -1
+    var maxMinute: Int = -1
+    var maxValue: Int = -1
+    for(g in sleepTimes.keys) {
+        for(t in 0..59) {
+            val sleepTime = sleepTimes[g]!![t]
+            if(sleepTime > maxValue) {
+                maxValue = sleepTime
+                maxGuard = g
+                maxMinute = t
             }
         }
     }
-    return minutes.indexOf(minutes.max())
+    return maxGuard * maxMinute
 }
+
 
 fun main() {
     val records = sortRecords(readInput())
     val sleepTimes = findGuardsSleepTime(records)
-    val guardWithMostTimeSlept = findGuardWithMostMinutesSlept(sleepTimes)
-    val mostSleepyMinute = findGuardsMostSleepyMinute(records, guardWithMostTimeSlept)
-    val result = mostSleepyMinute * guardWithMostTimeSlept
-    println(result)
+    val result = calculateResult(sleepTimes)
+    print(result)
 }
