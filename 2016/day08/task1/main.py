a18d5125240e0beffcd339028b3eb97321b05655
Lukasz1928 <Lukasz19281@gmail.com>
2020-08-05 15:05:37 +0200

    Solved 2016 day08

---
+++ 2016/day08/task1/main.py
@@ -0,0 +1,64 @@
+import re
+
+
+def parse_instruction(i):
+    pattern_rect = "rect (\\d+)x(\\d+)"
+    m_rect = re.match(pattern_rect, i)
+    if m_rect is not None:
+        return "rect", int(m_rect.group(1)), int(m_rect.group(2))
+
+    pattern_rc = "rotate column x=(\\d+) by (\\d+)"
+    m_rc = re.match(pattern_rc, i)
+    if m_rc is not None:
+        return "rc", int(m_rc.group(1)), int(m_rc.group(2))
+
+    pattern_rr = "rotate row y=(\\d+) by (\\d+)"
+    m_rr = re.match(pattern_rr, i)
+    if m_rr is not None:
+        return "rr", int(m_rr.group(1)), int(m_rr.group(2))
+
+
+def rect(d, w, h):
+    for wi in range(w):
+        for hi in range(h):
+            d[hi][wi] = 1
+    return d
+
+
+def rc(d, c, rots):
+    col = [d[i][c] for i in range(len(d))]
+    new_col = col[len(col) - rots:] + col[:len(col) - rots]
+    for i in range(len(d)):
+        d[i][c] = new_col[i]
+    return d
+
+
+def rr(d, r, rots):
+    row = d[r]
+    new_row = row[len(row) - rots:] + row[:len(row) - rots]
+    d[r] = new_row
+    return d
+
+
+def apply(d, i):
+    instr = parse_instruction(i)
+    if instr[0] == "rect":
+        return rect(d, instr[1], instr[2])
+    if instr[0] == "rc":
+        return rc(d, instr[1], instr[2])
+    if instr[0] == "rr":
+        return rr(d, instr[1], instr[2])
+    return d
+
+
+with open('input', 'r') as f:
+    instrs = [l.strip() for l in f]
+
+size = [50, 6]
+display = [[0] * size[0] for _ in range(size[1])]
+
+for i in instrs:
+    display = apply(display, i)
+
+result = sum([sum(x) for x in display])
+print(result)
--- README.md
+++ README.md
@@ -138,7 +138,7 @@ Solutions  of <cite>[Advent of Code][1]</cite> programming tasks.
 		<td rowspan="2">08</td>
 		<td>1</td>
 		<td><img src="https://symbols.getvecta.com/stencil_85/50_julia-language-icon.d9f53761e1.svg" width="48" height="48"/></td>
-		<td><img src="https://img.icons8.com/color/48/000000/delete-sign.png"/></td>
+		<td><img src="https://img.icons8.com/color/48/000000/python.png"></td>
 		<td><img src="https://img.icons8.com/color/48/000000/delete-sign.png"/></td>
 		<td><img src="https://img.icons8.com/color/48/000000/delete-sign.png"/></td>
 		<td><img src="https://img.icons8.com/color/48/000000/python.png"></td>
@@ -146,7 +146,7 @@ Solutions  of <cite>[Advent of Code][1]</cite> programming tasks.
 	<tr>
 		<td>2</td>
 		<td><img src="https://symbols.getvecta.com/stencil_85/50_julia-language-icon.d9f53761e1.svg" width="48" height="48"/></td>
-		<td><img src="https://img.icons8.com/color/48/000000/delete-sign.png"/></td>
+		<td><img src="https://img.icons8.com/color/48/000000/python.png"></td>
 		<td><img src="https://img.icons8.com/color/48/000000/delete-sign.png"/></td>
 		<td><img src="https://img.icons8.com/color/48/000000/delete-sign.png"/></td>
 		<td><img src="https://img.icons8.com/color/48/000000/python.png"></td>
