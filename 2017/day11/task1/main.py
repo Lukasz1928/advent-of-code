2f01d19576457f4e1c08c96cd637674150c37889
Lukasz1928 <Lukasz19281@gmail.com>
2020-08-12 13:27:58 +0200

    Solved 2017 day11

---
+++ 2017/day11/task1/main.py
@@ -0,0 +1,88 @@
+from collections import Counter
+
+
+def read_input():
+    with open('input', 'r') as f:
+        return [x for x in f.read().strip().split(',')]
+
+
+def remove_cancelling_moves(ms):
+    c = Counter(ms)
+    return {
+        'n': c['n'] - c['s'],
+        'ne': c['ne'] - c['sw'],
+        'se': c['se'] - c['nw']
+    }
+
+
+def reduce_moves(ms):
+    changed = True
+    reduced = False
+    while changed:
+        changed = False
+        if ms['n'] > 0:
+            if ms['se'] > 0:
+                m = min(ms['n'], ms['se'])
+                ms['n'] = ms['n'] - m
+                ms['se'] = ms['se'] - m
+                ms['ne'] = ms['ne'] + m
+                changed = True
+            elif ms['ne'] < 0:
+                m = min(ms['n'], -ms['ne'])
+                ms['n'] = ms['n'] - m
+                ms['ne'] = ms['ne'] + m
+                ms['se'] = ms['se'] - m
+                changed = True
+        if ms['n'] < 0:
+            if ms['ne'] > 0:
+                m = min(-ms['n'], ms['ne'])
+                ms['n'] = ms['n'] + m
+                ms['ne'] = ms['ne'] - m
+                ms['se'] = ms['se'] + m
+                changed = True
+            elif ms['se'] < 0:
+                m = min(-ms['n'], -ms['se'])
+                ms['n'] = ms['n'] + m
+                ms['se'] = ms['se'] + m
+                ms['ne'] = ms['ne'] - m
+                changed = True
+        if ms['ne'] > 0:
+            if ms['se'] < 0:
+                m = min(ms['ne'], -ms['se'])
+                ms['ne'] = ms['ne'] - m
+                ms['se'] = ms['se'] + m
+                ms['n'] = ms['n'] + m
+                changed = True
+        if ms['ne'] < 0:
+            if ms['se'] > 0:
+                m = min(-ms['ne'], ms['se'])
+                ms['ne'] = ms['ne'] + m
+                ms['se'] = ms['se'] - m
+                ms['n'] = ms['n'] - m
+                changed = True
+        if ms['se'] > 0:
+            if ms['ne'] < 0:
+                m = min(ms['se'], -ms['ne'])
+                ms['se'] = ms['se'] - m
+                ms['ne'] = ms['ne'] + m
+                ms['n'] = ms['n'] - m
+                changed = True
+        if ms['se'] < 0:
+            if ms['ne'] > 0:
+                m = min(-ms['se'], ms['ne'])
+                ms['se'] = ms['se'] + m
+                ms['ne'] = ms['ne'] - m
+                ms['n'] = ms['n'] + m
+                changed = True
+        if changed:
+            reduced = True
+    return ms, reduced
+
+
+moves = read_input()
+c = True
+short_moves = remove_cancelling_moves(moves)
+while c:
+    short_moves, c = reduce_moves(short_moves)
+length = sum([abs(x) for x in short_moves.values()])
+print(length)
--- README.md
+++ README.md
@@ -192,7 +192,7 @@ Solutions  of <cite>[Advent of Code][1]</cite> programming tasks.
 		<td>1</td>
 		<td><img src="https://img.icons8.com/color/48/000000/c-programming.png"/></td>
 		<td><img src="https://img.icons8.com/color/48/000000/delete-sign.png"/></td>
-		<td><img src="https://img.icons8.com/color/48/000000/delete-sign.png"/></td>
+		<td><img src="https://img.icons8.com/color/48/000000/python.png"></td>
 		<td><img src="https://img.icons8.com/color/48/000000/delete-sign.png"/></td>
 		<td><img src="https://img.icons8.com/color/48/000000/python.png"></td>
 	</tr>
@@ -200,7 +200,7 @@ Solutions  of <cite>[Advent of Code][1]</cite> programming tasks.
 		<td>2</td>
 		<td><img src="https://img.icons8.com/color/48/000000/c-programming.png"/></td>
 		<td><img src="https://img.icons8.com/color/48/000000/delete-sign.png"/></td>
-		<td><img src="https://img.icons8.com/color/48/000000/delete-sign.png"/></td>
+		<td><img src="https://img.icons8.com/color/48/000000/python.png"></td>
 		<td><img src="https://img.icons8.com/color/48/000000/delete-sign.png"/></td>
 		<td><img src="https://img.icons8.com/color/48/000000/python.png"></td>
 	</tr>
