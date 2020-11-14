d233794141e12abe6f2cab8d7dfda64d790f4574
Lukasz1928 <Lukasz19281@gmail.com>
2020-11-14 16:59:53 +0100

    Solved 2018 day20

---
+++ solutions/2018/day20/task1/main.py
@@ -0,0 +1,38 @@
+# that's not a general solution that will work for any input
+# it assumes that every alternative path ends in the same place
+import networkx as nx
+
+
+def read_input():
+    with open('input', 'r') as f:
+        regex = f.read().strip()
+    return regex[1:-1]
+
+
+def move(loc, dir):
+    moves = {'N': lambda x, y: (x, y + 1),
+             'S': lambda x, y: (x, y - 1),
+             'E': lambda x, y: (x + 1, y),
+             'W': lambda x, y: (x - 1, y)}
+    return moves[dir](*loc)
+
+
+regex = read_input()
+graph = nx.Graph()
+locs = []
+loc = (0, 0)
+for direction in regex:
+    if direction == '(':
+        locs.append(loc)
+    elif direction == ')':
+        loc = locs.pop()
+    elif direction == '|':
+        loc = locs[-1]
+    else:
+        new_loc = move(loc, direction)
+        graph.add_edge(loc, new_loc)
+        loc = new_loc
+shortest_paths = nx.single_source_shortest_path_length(graph, (0, 0))
+lengths = shortest_paths.values()
+result = max(lengths)
+print(result)
